08833a795e13f640775239d02179e4040068e224
joeliquite <106636455+joeliquite@users.noreply.github.com>
2023-05-31 20:43:43 -0600

    Update Transaccion.java

--- atm/src/Transaccion.java
+++ atm/src/Transaccion.java
@@ -8,21 +8,34 @@
  * @author Yoel
  */
 public class Transaccion {
-    private int numeroCuenta ;
+     
+    // Como los atributos son privados usaremos los Get para que sean heredados de las subclases
     
-  
     
-    //metodos para numero de cuenta
-  public  void   numeroCuenta(){
-        
-    }  
-  
-    //metodo ejecutar
-  public void ejecutar(){
-      
-  }
-  
-  
+    private int numeroCuenta;
+    private BaseDatosBanco baseDatosBanco;// requiere acceso a las cuentas que esta asociada
+    
+// constructor
+    // la instancia super se usara unicamente en las subclases
+
+public Transaccion(int numeroCuenta, BaseDatosBanco baseDatosBancoATM){
+    this.numeroCuenta = numeroCuenta;
+    baseDatosBanco = baseDatosBancoATM;
+}    // fin constructor
+
+// metodos
+
+    public int obtenerNumeroCuenta() {
+        return numeroCuenta;
+    }
+    // necestiamos una referencia a la base de datos
+    public BaseDatosBanco obtenerBaseDatosBanco(){
+        return baseDatosBanco;
+    }
+    
+    // Los metedos abstractos son porque no se pueden crear instancias de las clases abstractas
+    // esto obliga a las subclases a heredarlos y sobreescribirlos para implementarlos. 
+    abstract public  void ejecutar();
     
 }
 
