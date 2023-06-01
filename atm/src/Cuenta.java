e6bebdd4fd5635e4836063fdac84c602f13cb99b
joeliquite <106636455+joeliquite@users.noreply.github.com>
2023-05-31 20:34:00 -0600

    Update Cuenta.java

--- atm/src/Cuenta.java
+++ atm/src/Cuenta.java
@@ -8,18 +8,53 @@
  * @author Yoel
  */
 public class Cuenta {
-    private int numeroCuenta;
+       private int numeroCuenta;
     private int nip;
-    private double saldoDisponible;
-    private double SaldoTotal;
+    private double saldoDisponible; // disponible para retirar
+    private double saldoTotal; // disponible mas depositos
     
-    
-    
-    public boolean validarNip(){
-        
+    //Constructor
+    public Cuenta(int numeroCuenta, int nip, double saldoDisponible, double saldoTotal) {
+        this.numeroCuenta = numeroCuenta;
+        this.nip = nip;
+        this.saldoDisponible = saldoDisponible;
+        this.saldoTotal = saldoTotal;
     }
     
-    //metodo
-   public 
+    // Validar NIP
+    public boolean validarNIP( int nipUsuario ){
+         if(nipUsuario == nip){
+             return true;
+         }else{
+             return false;
+         }
+     }
+    
+    // metodos
+    public boolean validarNip(){
+        return true;
+    }
+    
+    public double obtenerSaldoDisponible(){
+        return saldoDisponible;
+    }
+    
+    public double obtenerSaldoTotal(){
+        return saldoTotal;
+    }
+    
+    
+    public void abonar(double monto){
+        saldoTotal += monto;
+    }
+    
+    public void cargar(double monto){
+        saldoDisponible -= monto;
+        saldoTotal -= monto; 
+    }
+    
+    public int obtenerNumeroCuenta(){
+        return numeroCuenta;
+    }
     
 }
