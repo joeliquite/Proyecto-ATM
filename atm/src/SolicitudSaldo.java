1eca5dbfef2dd235311fc42447f730f377889659
joeliquite <106636455+joeliquite@users.noreply.github.com>
2023-05-31 20:42:50 -0600

    Update SolicitudSaldo.java

--- atm/src/SolicitudSaldo.java
+++ atm/src/SolicitudSaldo.java
@@ -7,11 +7,39 @@
  *
  * @author Yoel
  */
-public class SolicitudSaldo {
+public class SolicitudSaldo extends Transaccion {
     
-    
-    //metodo ejecutar saldo
-    public void ejecutar(){
+    // Constructor
+    //  mismos atributos que en la clase padre
+    public SolicitudSaldo(int numeroCuentaUsuario, BaseDatosBanco baseDatosBancoATM){
+        
+        // usamos la sentencia super para indicar que los que vamos a inicializar son los de la clase padre 
+        super(numeroCuentaUsuario, baseDatosBancoATM);
         
     }
+    
+    // metodos heredado que es abstracto 
+    // al ser abstracto podemos suscribirlo e implementar una funcionalidad propia de solicitudSaldo
+    @Override
+    public void ejecutar(){
+        // Crear referencias a la base de Datos usando el metodo que implementamos 
+        BaseDatosBanco baseDatosBanco = obtenerBaseDatosBanco();
+        
+        // obtener el sado disponible de la cuenta indicada
+        
+        double saldoDisponible = baseDatosBanco.obtenerSaldoDisponible(obtenerNumeroCuenta());
+        
+        double saltoTotal = baseDatosBanco.obtenerSaldoTotal(obtenerNumeroCuenta());
+        
+        
+        // mostrar en pantalla
+        
+        System.out.println("Saldos");
+        System.out.println("Saldo Disponible");
+        System.out.println(saldoDisponible);
+        System.out.println("Saldo Total");
+        System.out.println(saltoTotal);
+    }
+    
+    
 }
