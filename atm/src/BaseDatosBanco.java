094365c0a34a6a53942f4a5da6c810f20437bc36
joeliquite <106636455+joeliquite@users.noreply.github.com>
2023-05-31 20:31:17 -0600

    Update BaseDatosBanco.java

--- atm/src/BaseDatosBanco.java
+++ atm/src/BaseDatosBanco.java
@@ -9,34 +9,60 @@
  */
 public class BaseDatosBanco {
     
-    //metodo para evaluar el usario
-    public boolean autenticarUsuario(){
+       
+    private Cuenta cuentas[];
+    
+    
+   
+    
+    public BaseDatosBanco(){
+        cuentas = new Cuenta[2];
+        // se pasan los atributos como parate de cada cuenta por eso se crea una referencia para cada uno 
+        cuentas[ 0 ] = new Cuenta( 12345, 54321, 1000.0, 1200.0 );
+        cuentas[ 1 ] = new Cuenta( 98765, 56789, 200.0, 200.0 );
+    }
+    
+    // metodos
+    
+    private Cuenta obtenerCuenta(int numeroCuenta){
+        // tipo, variable arreglo
+        for(Cuenta cuentaActual: cuentas){
+            if(cuentaActual.obtenerNumeroCuenta() == numeroCuenta){
+                return cuentaActual;
+            }
+            
+        }
+        return null; // si no encuentra la cuenta solamente retorna null 
+    }
+    
+    public boolean autenticarUsuario(int numeroCuentaUsuario, int nipUsuario){
+        Cuenta cuentaUsuario = obtenerCuenta(numeroCuentaUsuario);
         
+        if(cuentaUsuario != null)
+            return cuentaUsuario.validarNIP( nipUsuario );
+        else
+            return false;
     }
     
     
-    //metodo 
-    public double ObtenerSaldoDisponible(){
+    public double obtenerSaldoDisponible(int numeroCuentaUsuario){
         
-    }
-    
-    //metodo
-    public double SaldoTotal(){
-        
-    }
-    
-    // metodo abonar
-    public  void abonar (){
-        
+        return obtenerCuenta( numeroCuentaUsuario ).obtenerSaldoDisponible();
     }
     
     
-    //metodo cargar
-    public void cargar(){
+    public double obtenerSaldoTotal(int numeroCuentaUsuario){
         
+        return obtenerCuenta( numeroCuentaUsuario ).obtenerSaldoTotal();
     }
     
+    public void abonar(int numeroCuentaUsuario, double monto){
+        obtenerCuenta( numeroCuentaUsuario ).abonar( monto );
+    }
     
+    public void cargar(int numeroCuentaUsuario, double monto){
+        obtenerCuenta( numeroCuentaUsuario ).cargar( monto );
+    }
     
     
 }
